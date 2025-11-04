a6f761f6cead68980056c01427cb32ffdc08cb28
rgulechha2105-cloud <rgulechha2105@gmail.com>
2025-11-05 00:00:04 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,11 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const db = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 3306, // Always 3306 for MySQL
-  ssl: { rejectUnauthorized: true } // Needed for Clever Cloud remote access
 });
 
-module.exports = db;
+module.exports = pool;
