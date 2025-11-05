4affe5840dce2654b0fd9afdfe2162a4be9c3803
Rishabh Gulechha <rgulechha2105@gmail.com>
2025-11-05 10:28:27 +0530

    Resolved merge conflict in db.js

--- db.js
+++ db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
@@ -10,16 +9,3 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool;
-=======
-const mysql = require("mysql2/promise");
-require("dotenv").config();
-
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-
-module.exports = pool;
->>>>>>> fab245236268b59d187d02d42416883c54a7ac0f
